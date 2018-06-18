9e902b4b38e208722bdbdeb589519c79e9009c37
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2018-06-17 17:10:34 -0700

    finished component checkpoint for Bloc

---
+++ src/components/ToDo.js
@@ -0,0 +1,18 @@
+//import React and React's Component class
+import React, { Component } from 'react';
+
+//Define a class that extends component
+class ToDo extends Component {
+
+
+//Define a render() method on your class, this method is REQUIRED
+  render() {
+    return (
+      <li> A todo will go here </li>
+    );
+  }
+}
+
+export default ToDo;
+
+
